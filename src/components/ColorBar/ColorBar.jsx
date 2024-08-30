733640c06306e60605d546c9bdf544ecf77e3ca2
Daniel <npm030312@gmail.com>
2024-08-30 16:40:25 -0600

    No poder eliminar con solo 2 colores

--- src/components/ColorBar/ColorBar.jsx
+++ src/components/ColorBar/ColorBar.jsx
@@ -3,11 +3,11 @@ import styles from "./ColorBar.module.css"
 import Opcion from "../opcion/Opcion";
 export default function ColorBar({bgColor, textColor, name, setAlert, eliminar, cantidad}){
     const [visibleOpciones, setVisibleOpciones] = useState(false)
-    console.log(name)
+    console.log(cantidad)
     return(
         <div className={styles.ColorBar} style={{backgroundColor:bgColor, width:`calc(100vw / ${cantidad})`}} onMouseOver={()=>setVisibleOpciones(true)} onMouseLeave={()=>setVisibleOpciones(false)}>
             {visibleOpciones && <div className={styles.opcionesBar}>
-            <Opcion textColor={textColor} color={bgColor} setAlert={setAlert} eliminar={eliminar} />   
+            {cantidad>2 && <Opcion textColor={textColor} color={bgColor} setAlert={setAlert} eliminar={eliminar} /> }
             <Opcion textColor={textColor} color={bgColor} setAlert={setAlert} />  
             </div>}
             
